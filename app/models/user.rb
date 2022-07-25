7dcafcfcad2190f7ff4da686b18c6cf36c0bcb65
Charlie Bertrand <charlie.bertrand@live.com>
2022-07-25 15:48:56 +0200

    set devise for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+
+  # default is :
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :validatable
+
+#   We just need users in our seeds and a way to log in. Therefore, we will disable all the Devise features for our User model except two of them:
+
+  # The feature to sign in users (:database_authenticatable)
+  # The feature to validate the email and password using Devise built-in validations (:validatable)
+  devise :database_authenticatable, :validatable
 end
