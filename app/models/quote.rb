5bfc7a52a6130c1c6864e19ec495554f10315005
Charlie Bertrand <charlie.bertrand@live.com>
2022-07-25 16:31:15 +0200

    adding security broadcast

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -10,7 +10,7 @@ class Quote < ApplicationRecord
   # after_update_commit -> { broadcast_replace_later_to "quotes" }
   # after_destroy_commit -> { broadcast_remove_to "quotes" }
   # Those three callbacks are equivalent to the following single line
-  broadcasts_to ->(quote) { 'quotes' }, inserts_by: :prepend
+  broadcasts_to ->(quote) { [quote.company, 'quotes'] }, inserts_by: :prepend
 
   # after_create_commit -> { broadcast_prepend_later_to 'quotes' }
   # # after_create_commit -> { broadcast_prepend_to 'quotes' }
