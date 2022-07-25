284e8769924a82260a15e4ab2e601a0fdd720fc2
Charlie Bertrand <charlie.bertrand@live.com>
2022-07-25 14:55:22 +0200

    finishing turbo stream setup for quotes

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -4,4 +4,26 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+
+  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  # after_update_commit -> { broadcast_replace_later_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+  # Those three callbacks are equivalent to the following single line
+  broadcasts_to ->(quote) { 'quotes' }, inserts_by: :prepend
+
+
+
+  # after_create_commit -> { broadcast_prepend_later_to 'quotes' }
+  # # after_create_commit -> { broadcast_prepend_to 'quotes' }
+  # # the above line commented is equivalent to :
+  # # after_create_commit lambda {
+  # #                       broadcast_prepend_to 'quotes',
+  # #                                            partial: 'quotes/quote',
+  # #                                            locals: { quote: self }
+  # #                     }
+
+  # # after_update_commit -> { broadcast_replace_to 'quotes' }
+  # after_update_commit -> { broadcast_replace_later_to 'quotes' }
+  # after_destroy_commit -> { broadcast_remove_to 'quotes' }
 end
