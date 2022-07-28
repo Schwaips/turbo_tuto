b96f21fa48177150f074fb0f12ee039cbaa9dbd0
Charlie Bertrand <charlie.bertrand@live.com>
2022-07-28 12:42:58 +0200

    create turbo stream for line items dates

--- app/controllers/line_item_dates_controller.rb
+++ app/controllers/line_item_dates_controller.rb
@@ -12,14 +12,16 @@ class LineItemDatesController < ApplicationController
     @line_item_date = @quote.line_item_dates.build(line_item_date_params)
 
     if @line_item_date.save
-      redirect_to quote_path(@quote), notice: "Date was successfully created."
+      respond_to do |format|
+        format.html { redirect_to path quote_path(@quote), notice: 'Date was successfully created.' }
+        format.turbo_stream { flash.now[:notice] = 'Date was successfully created'}
+      end
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if @line_item_date.update(line_item_date_params)
--- app/models/line_item_date.rb
+++ app/models/line_item_date.rb
@@ -4,4 +4,10 @@ class LineItemDate < ApplicationRecord
   validates :date, presence: true, uniqueness: { scope: :quote_id }
 
   scope :ordered, -> { order(date: :asc) }
+
+  # broadcasts_to ->(quote) { [quote.company, 'quotes'] }, inserts_by: :prepend
+
+  def previous_date
+    quote.line_item_dates.ordered.where('date < ?', date).last
+  end
 end
