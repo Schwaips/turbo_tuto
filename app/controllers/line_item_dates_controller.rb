282b3365a422e548a6811e9faee1af5a2e56ab30
Charlie Bertrand <charlie.bertrand@live.com>
2022-07-28 13:16:26 +0200

    test for line items dates system

--- app/controllers/line_item_dates_controller.rb
+++ app/controllers/line_item_dates_controller.rb
@@ -14,7 +14,7 @@ class LineItemDatesController < ApplicationController
     if @line_item_date.save
       respond_to do |format|
         format.html { redirect_to path quote_path(@quote), notice: 'Date was successfully created.' }
-        format.turbo_stream { flash.now[:notice] = 'Date was successfully created'}
+        format.turbo_stream { flash.now[:notice] = 'Date was successfully created' }
       end
     else
       render :new, status: :unprocessable_entity
@@ -25,7 +25,10 @@ class LineItemDatesController < ApplicationController
 
   def update
     if @line_item_date.update(line_item_date_params)
-      redirect_to quote_path(@quote), notice: "Date was successfully updated."
+      respond_to do |format|
+        format.html { redirect_to path quote_path(@quote), notice: 'Date was successfully updated.' }
+        format.turbo_stream { flash.now[:notice] = 'Date was successfully updated' }
+      end
     else
       render :edit, status: :unprocessable_entity
     end
@@ -34,7 +37,10 @@ class LineItemDatesController < ApplicationController
   def destroy
     @line_item_date.destroy
 
-    redirect_to quote_path(@quote), notice: 'Date was successfully destroyed.'
+    respond_to do |format|
+      format.html { redirect_to quote_path(@quote), notice: 'Date was successfully destroyed.' }
+      format.turbo_stream { flash.now[:notice] = 'Date was successfully destroyed.' }
+    end
   end
 
   private
@@ -50,5 +56,4 @@ class LineItemDatesController < ApplicationController
   def set_quote
     @quote = current_company.quotes.find(params[:quote_id])
   end
-
 end
